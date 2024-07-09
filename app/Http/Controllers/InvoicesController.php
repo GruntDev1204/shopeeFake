2d960cd60ffb771bbf91631ffbf5f0d03bf50e79
= <hotrung6935@gmail.com>
2024-07-10 02:52:49 +0700

    BackEnd Shopee Fake Function 70%

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -7,6 +7,7 @@ use App\Models\Invoices;
 use App\Models\Product;
 use App\Models\Transports;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class InvoicesController extends Controller
@@ -26,19 +27,19 @@ class InvoicesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create($codeCustomer ,$codeTransport)
+    public function create($codeCustomer, $codeTransport)
     {
         try {
 
             $dataCart = Carts::join('products', 'products.codeProduct', '=', 'carts.codeProduct')
-            ->select('products.name', 'products.price', 'products.promotionalPrice', 'products.UrlPhoto', 'products.Rate', 'carts.*')
-            ->where('carts.codeCustomer', $codeCustomer)
-            ->where('products.action', true)
-            ->get();
+                ->select('products.name', 'products.price', 'products.promotionalPrice', 'products.UrlPhoto', 'products.Rate', 'carts.*')
+                ->where('carts.codeCustomer', $codeCustomer)
+                ->where('products.action', true)
+                ->get();
 
             $dataTransport = Transports::where('UnitCode', $codeTransport)
-            ->select('price', 'PerCentPrice', 'Name')
-            ->first();
+                ->select('price', 'PerCentPrice', 'Name')
+                ->first();
 
             if ($dataCart->isEmpty()) {
                 return response()->json(['status' => 'Data not found'], 404);
@@ -62,7 +63,7 @@ class InvoicesController extends Controller
 
             $invoice =  Invoices::create([
 
-                'codeInvoices' =>Str::uuid(),
+                'codeInvoices' => Str::uuid(),
                 'totalPay' => $PriceInvoices,
                 'realityPay' => $RelityPriceInvoice,
 
@@ -76,12 +77,10 @@ class InvoicesController extends Controller
                         'codeInvoice' => $invoice->codeInvoices,
                     ]);
                 }
-
             }
             return response()->json([
                 'status' => 'Invoice created updated successfully!',
             ], 200);
-
         } catch (\Exception  $e) {
             return response()->json(['status' => 'An error occurred', 'message' => $e->getMessage()], 500);
         }
@@ -93,9 +92,28 @@ class InvoicesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function allInvoices()
     {
-        //
+        try {
+
+            $Login = Auth::guard('admins')->user();
+
+            if (!$Login) {
+                return response()->json(['status' => 'Admin not authenticated'], 401);
+            } else {
+                $dataInvoice = Carts::join('invoices', 'carts.codeInvoice', '=', 'invoices.codeInvoices')
+                    ->where('carts.status', true)
+                    ->select('carts.*', 'invoices.is_done' , 'invoices.status')
+                    ->get();
+
+                return response()->json([
+                    'dataInvoice' => $dataInvoice
+                ], 200);
+
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'An error occurred', 'message' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -104,9 +122,19 @@ class InvoicesController extends Controller
      * @param  \App\Models\Invoices  $invoices
      * @return \Illuminate\Http\Response
      */
-    public function show(Invoices $invoices)
+    public function showMyInvoice($codeCustomer)
     {
-        //
+
+        $dataInvoice = Carts::join('invoices', 'carts.codeInvoice', '=', 'invoices.codeInvoices')
+        ->where('invoices.codeCustomer' , $codeCustomer)
+        ->where('carts.status', true)
+        ->select('carts.*', 'invoices.is_done' , 'invoices.status')
+        ->get();
+
+        return response()->json([
+            'dataInvoice' => $dataInvoice
+        ], 200);
+
     }
 
     /**
@@ -115,9 +143,80 @@ class InvoicesController extends Controller
      * @param  \App\Models\Invoices  $invoices
      * @return \Illuminate\Http\Response
      */
-    public function edit(Invoices $invoices)
+    public function editStatus($codeInvoice)
     {
-        //
+
+        try {
+
+            $Login = Auth::guard('admins')->user();
+
+            if (!$Login) {
+
+                return response()->json(['status' => 'Admin not authenticated'], 401);
+
+            } else {
+
+
+                $Data = Invoices::where('codeInvoices' , $codeInvoice)->first();
+
+                if(!$Data){
+                 return response()->json(['status' => 'Data Invoices not found!'], 404);
+                }
+
+                $Data->status = ! $Data->status;
+                $Data->save();
+
+
+                return response()->json([
+
+                    'status' => $Data->status ? 'Status of this Invoice has been changed to Is Confirmed! ' :  'Status of this Invoice has been changed to Is Not Confirmed! '
+
+                ], 200);
+            }
+
+        }catch (\Exception $e) {
+            return response()->json(['status' => 'An error occurred', 'message' => $e->getMessage()], 500);
+        }
+
+    }
+
+
+
+    public function editDoneStatus($codeInvoice)
+    {
+
+        try {
+
+            $Login = Auth::guard('admins')->user();
+
+            if (!$Login) {
+
+                return response()->json(['status' => 'Admin not authenticated'], 401);
+
+            } else {
+
+
+                $Data = Invoices::where('codeInvoices' , $codeInvoice)->first();
+
+                if(!$Data){
+                 return response()->json(['status' => 'Data Invoices not found!'], 404);
+                }
+
+                $Data->is_done = ! $Data->is_done;
+                $Data->save();
+
+
+                return response()->json([
+
+                    'status' => $Data->is_done ? 'Status of this Invoice has been changed to Is Done ' :  'Status of this Invoice has been changed to Is Not Done! '
+
+                ], 200);
+            }
+
+        }catch (\Exception $e) {
+            return response()->json(['status' => 'An error occurred', 'message' => $e->getMessage()], 500);
+        }
+
     }
 
     /**
@@ -138,8 +237,38 @@ class InvoicesController extends Controller
      * @param  \App\Models\Invoices  $invoices
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Invoices $invoices)
+    public function cancelByAdmin($id)
     {
-        //
+        try {
+
+            $Login = Auth::guard('admins')->user();
+
+            if (!$Login) {
+
+                return response()->json(['status' => 'Admin not authenticated'], 401);
+
+            } else {
+
+
+                $Data = Invoices::find($id);
+
+                if(!$Data){
+                 return response()->json(['status' => 'Data Invoices not found!'], 404);
+                }
+
+                $Data->delete();
+                $Data->save();
+
+
+                return response()->json([
+                    'status' => "deleted Invoice" + $id +  "successfully!!"
+
+                ], 200);
+            }
+
+        }catch (\Exception $e) {
+            return response()->json(['status' => 'An error occurred', 'message' => $e->getMessage()], 500);
+        }
+
     }
 }
